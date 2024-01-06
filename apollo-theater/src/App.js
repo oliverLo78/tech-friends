cc07e30aeac0210fa9b63aad5be4e8f887565b2a
Oliver Lopez <oliverberto@gmail.com>
2024-01-06 16:02:26 -0500

    implemented useState useEffect hook

--- apollo-theater/src/App.js
+++ apollo-theater/src/App.js
@@ -1,19 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
-const [firstReview, second] = [
-  "An amazing story of love and adventure.",
-  "Absolutely riveting!",
-  "I loved this movie."
-];
+function App() {
+  const [ review, setReview ] = useState("An amazing story of love and adventure.");
+  const [ rating, setRating ] = useState(10);
 
-console.log(firstReview);
-console.log(second);
+  useEffect(() => {
+    console.log(`Review rignt now is ${review}!`);
+  }, [review, rating]);
 
-function App({ library }) {
+  useEffect(() => {
+    console.log(`Rating right now is ${rating}!`);
+  }, [rating]);
+  
   return (
     <div className="App">
-      <h1>Hello from {library}</h1>
+      <h1>Current review is {review}</h1>
+      <button onClick={() => setReview("a bad movie.")}>
+        Bad Review
+      </button>
+      <button onClick={() => setReview("a great movie.")}>
+        Great Review
+      </button>
+      <h2>Current rating is {rating}.
+      </h2>
+      <button onClick={() => setRating(rating + 1)}>
+        Increase Rating
+      </button>
+      <button onClick={() => setRating(rating - 1)}>
+        Decrease Rating
+      </button>
     </div>
   );
 }
